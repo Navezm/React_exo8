bd2856f24038b52f39ba42990e69cdf83b804ce5
Navezm <navez.martin@gmail.com>
2021-01-14 12:52:35 +0100

    Exo finished

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ class App extends Component {
   bjr = () => {
     console.log("Bonjour");
   };
+
+  state = {
+    nombre: 1
+  }
   
+  number = (e) => {
+    console.log(e.target);
+  }
+
   render(){
     return (
       <div className="App">
-        <h1>Exercice 8</h1>
+        <h1 onMouseOver={this.number}>Exercice 8</h1>
         <button onDoubleClick={this.bjr}>Click On Me</button>
       </div>
     );
